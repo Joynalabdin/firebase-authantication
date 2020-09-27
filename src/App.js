74156c7f4b445e77f32ca507bb6db7268beee65a
Md Joynal Abdin Robel <robel.joynal@gmail.com>
2020-09-27 15:58:34 +0600

    google authentication

--- src/App.js
+++ src/App.js
@@ -1,24 +1,98 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
+  const [user, setUser] = useState({
+    isSignedIn: false,
+    name: '',
+    email: '',
+    password: '',
+    photo: ''
+  })
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  const handleSignIn = () => {
+    firebase.auth().signInWithPopup(provider)
+    .then(res=>{
+      const {displayName,email,photoURL} = res.user;
+      const signInUser = {
+        isSignedIn: true,
+        name: displayName,
+        email: email,
+        photo: photoURL
+      }
+      setUser(signInUser)
+    })
+    .catch(err =>{
+      console.log(err)
+      console.log(err.message)
+    })
+  }
+  const handleSignOut = () => {
+    const signOutUser ={
+      isSignedIn : false,
+      name : '',
+      email : '',
+      photoURL : ''
+    }
+    setUser(signOutUser)
+  }
+  const handleSubmit = () => {
+
+  }
+  const handleBlur = (e) => {
+    let isFormValid = true;
+    if (e.target.name === 'email') {
+      isFormValid = /\S+@\S+\.\S+/.test(e.target.value);
+      
+    }
+    if (e.target.name === 'password') {
+      const isPasswordValild = e.target.value.length > 6;
+      const passwordHasNumber = /\d{1}/.test(e.target.value)
+      isFormValid = isPasswordValild&&passwordHasNumber;
+    }
+    if (isFormValid) {
+      const newUserInfo = {...user}
+      newUserInfo[e.target.name] = e.target.value;
+      setUser(newUserInfo)
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        user.isSignedIn  ?
+        <button onClick={handleSignOut} >Sign Out</button>
+        :
+        <button onClick={handleSignIn} >Sign In</button>
+
+      }
+      {
+        user.isSignedIn && 
+        <div>
+          <p> Welcome {user.name} </p>
+          <p> Your email:{user.email} </p>
+          <img src={user.photo} alt=""/>
+        </div>
+      }
+      <h1>Our own Authentication</h1>
+      <p> Your name: {user.name} </p>
+      <p>Email:{user.email} </p>
+      <p>Password:{user.password} </p>
+
+
+
+      <form onSubmit={handleSubmit}>
+      <input type="text" name="name" onBlur={handleBlur} placeholder="Your name" required/><br/>
+      <input type="text" name="email" onBlur={handleBlur} placeholder="Your email" required/><br/>
+      <input type="password" name="password" onBlur={handleBlur} placeholder="Your Password" required/><br/>
+      <input type="submit" value="Submit"/>   
+      </form>
+
     </div>
   );
 }
---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDUCz-oDygd4dG0f4DL2wkYCGy_bAhGI9E",
+    authDomain: "simple-three.firebaseapp.com",
+    databaseURL: "https://simple-three.firebaseio.com",
+    projectId: "simple-three",
+    storageBucket: "simple-three.appspot.com",
+    messagingSenderId: "1026450892782",
+    appId: "1:1026450892782:web:eed426b50f9cb64f3850ff"
+  };
+  export default firebaseConfig;
